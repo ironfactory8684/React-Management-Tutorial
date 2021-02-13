9a65e455ee664c321025939ee83d7016ec427e8c
ironfactory <dream7special@gmail.com>
2021-02-13 11:28:19 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customers = [{
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name' : '홍길동',
+  'birthday': '961122',
+  'gender': '남자',
+  'job': '대학생',
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name' : '신사임당',
+  'birthday': '940627',
+  'gender': '여자',
+  'job': '교수',
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name' : '별주부',
+  'birthday': '980231',
+  'gender': '간성',
+  'job': '교직원',
+}]
 class App extends Component {
   render() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2> Let's develop management sytem!</h2>
+    <div>
+      {customers.map(c => {  return (<Customer key={c.id} id={c.id} img={c.image} name={c.name}
+            birthday={c.birthday} gender={c.gender} job={c.job}/>)})}
     </div>
   );
 }
